6bfa19727dc467f3fbe5aaf36aec72a204c76cd9
PonorogoCreativeIT <ponorogocreativeit@gmail.com>
2020-02-04 15:52:59 +0700

    disable zoom

--- injectedHtml/index.js
+++ injectedHtml/index.js
@@ -1,23 +1,28 @@
 var content = script =>
   `<html>
-    <style>
-    *
-    {margin:0;padding:0;}
+    <head>
+      <style>
+        *
+        {margin:0;padding:0;}
 
-    canvas
-    {
-      position:absolute;transform:translateZ(0);
-      /* In case the React Transformation is not performant, we'll fall back to this one
+        canvas
+        {
+          position:absolute;transform:translateZ(0);
+          /* In case the React Transformation is not performant, we'll fall back to this one
 
-      transform-origin:left top;
-      -ms-transform-origin:left top;
-      -webkit-transform-origin:left top;
-      transform:rotate(-90deg) translate(-100%, 0px);
-      -ms-transform:rotate(-90deg)  translate(-100%, 0px);
-      -webkit-transform:rotate(-90deg)  translate(-100%, 0px);*/
-    }
-
-    </style>
+          transform-origin:left top;
+          -ms-transform-origin:left top;
+          -webkit-transform-origin:left top;
+          transform:rotate(-90deg) translate(-100%, 0px);
+          -ms-transform:rotate(-90deg)  translate(-100%, 0px);
+          -webkit-transform:rotate(-90deg)  translate(-100%, 0px);*/
+        }
+      </style>
+      <meta 
+        name='viewport' 
+        content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' 
+      />
+    </head>
     <body>
       <canvas style="margin-left: 0; margin-top: 0;"></canvas>
       <script>
